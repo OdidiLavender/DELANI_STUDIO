5b0aaefcaeb29bdd66737d0d05c6fa0af0235d11
OdidiLavender <odidilavender@gmail.com>
2022-03-14 16:29:10 +0300

    added the the sevices functions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//services 
+$(document).ready(function(){
+    var services =[{img:"design-img",info:"design-info"},{img:"dev-img",info:"dev-info"},{img:"product-img",info:"product-info"},]
+    
+    services.forEach(function(service){
+        $("." +service.img).click(function(){
+            $("."+service.info).show();
+            $("."+service.img).hide();
+        });
+        $("."+service.info).click(function(){
+            $("."+service.img).show();
+            $("."+service.info).hide();
+        });
+    })
+});
